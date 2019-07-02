f132d74b485bfdfbf4fa3802c26f9931dfa59e11
Tiago Knipers <tiagoknipers@MacBook-Air-de-Tiago.local>
2019-07-01 22:04:53 -0300

    first frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { SharedService } from './services/shared.service';
+import { TicketService } from './services/ticket.service';
+import { UserService } from './services/user.service';
+import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/security/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routes
   ],
-  providers: [],
+  providers: [UserService, TicketService, SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
